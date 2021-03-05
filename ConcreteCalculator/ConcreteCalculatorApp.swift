baea99ab08d585ca08ff05377495d8013d84f724
lcs-dsmith <dsmith@lcs.on.ca>
2021-03-05 11:18:43 -0500

    Initial Commit

---
+++ ConcreteCalculator/ConcreteCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcreteCalculatorApp.swift
+//  ConcreteCalculator
+//
+//  Created by Dylan Smith on 2021-03-05.
+//
+
+import SwiftUI
+
+@main
+struct ConcreteCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
